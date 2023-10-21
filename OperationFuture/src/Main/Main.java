74d2f39326b3feba9fceb6827199e97ebcb467f4
looke <looke@elrefaey>
2023-10-21 13:39:27 +0400

    Added animateText Function

--- OperationFuture/src/Main/Main.java
+++ OperationFuture/src/Main/Main.java
@@ -1,16 +1,27 @@
 package Main;
 
-public class Main {
+public class Main 
+{
 
-	public static void main(String[] args) {
+	public static void animateText( String text ) throws InterruptedException 
+	{
+		for( int i=0; i<text.length(); i++ ) 
+		{
+			System.out.print( text.charAt(i) );
+			Thread.sleep(100);
+		}
+	}
+	
+	public static void main(String[] args) throws InterruptedException 
+	{
 		
-		System.out.println("Here the game stars :)\n");
+		animateText("Traveling to Sahara Desert...\r");
+		//animateText("Traveling to Sahara Desert...\r");
+		//Guardian g1 = new Guardian();
 		
-		Guardian g1 = new Guardian();
+		//g1.build();
 		
-		g1.build();
-		
-		g1.getRating();
+		//g1.getRating();
 
 	}
 
